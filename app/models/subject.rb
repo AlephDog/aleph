c5f5a42ce9b7daeb06962984cc56d89975984fc3
Zuriel <zurieltv@ciencias.unam.mx>
2017-04-01 12:10:15 -0600

    Relaciones entre modelos existentes completa

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,4 @@
 class Subject < ApplicationRecord
-	has_many :professors,through: :proffesor_subjects
+	has_many :professors,through: :professor_subjects
+	has_many :professor_subjects,dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+professor = Professor.create(
+	fullname: 'Even Zuriel Torres Villegas',
+	username: 'Zuriel Torres',
+	email: 'zurieltv@ciencias.unam.mx',
+	password_digest: 'password',
+	like: 10000,
+	dislike: 0,
+	help: 9.4,
+	clarity: 10,
+	grade: 11,
+	school: 'UNAM',
+	biography: 'Biografía',
+	interests: 'Intereses'
+	)
+
+subject = Subject.create(
+	name: 'Matemáticas',
+	description: 'Lorem Ipsum',
+	area: 'Ciencias exactas'
+	)
+
+professor_subject = ProfessorSubject.create(
+	professor: professor,
+	subject: subject
+	)
